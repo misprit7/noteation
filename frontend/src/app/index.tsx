5b36bdaa6cdb5f263868c00c29980655f09e9a3c
misprit7 <xandernaumenko@gmail.com>
2022-09-17 13:18:24 -0700

    Got start page up

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -16,22 +16,37 @@ import { HomePage } from './pages/HomePage/Loadable';
 import { StartPage } from './pages/StartPage/Loadable';
 import { useTranslation } from 'react-i18next';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#8e8e8e',
+      main: '#616161',
+      dark: '#373737',
+      contrastText: '#fff',
+    }
+  }
+});
+
 export function App() {
   const { i18n } = useTranslation();
   return (
-    <BrowserRouter>
-      <Helmet
-        titleTemplate="%s - Noteation"
-        defaultTitle="Noteation"
-        htmlAttributes={{ lang: i18n.language }}
-      >
-        <meta name="description" content="Noteation" />
-      </Helmet>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Helmet
+          titleTemplate="%s - Noteation"
+          defaultTitle="Noteation"
+          htmlAttributes={{ lang: i18n.language }}
+        >
+          <meta name="description" content="Noteation" />
+        </Helmet>
 
-      <Switch>
-        <Route exact path={process.env.PUBLIC_URL + '/'} component={StartPage} />
-      </Switch>
-      <GlobalStyle />
-    </BrowserRouter>
+        <Switch>
+          <Route exact path={process.env.PUBLIC_URL + '/'} component={StartPage} />
+        </Switch>
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
--- frontend/src/app/pages/StartPage/index.tsx
+++ frontend/src/app/pages/StartPage/index.tsx
@@ -2,25 +2,34 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import logo from './assets/logo.png';
+import background from './assets/background.png';
 import styled from 'styled-components/macro';
+import AudioFileIcon from '@mui/icons-material/AudioFile';
 
 export function StartPage() {
   return (
-    <>
+    <Bg>
       <Helmet>
         <title>Start</title>
       </Helmet>
-      <Container>
-        <h1>Hello world</h1>
-        <img src={logo} className="start-logo"/>
-        <Button variant="contained">Test</Button>
-      </Container>
-    </>
+      <Stack justifyContent="center" alignItems="center" spacing={10}>
+        <Img src={logo}/>
+        <Button variant="contained" style={{minWidth: 100}}><AudioFileIcon/></Button>
+      </Stack>
+    </Bg>
   );
 }
 
-const img = styled.a`
-  width: 100px;
+const Img = styled.img`
+  width: 40%;
 `;
+
+const Bg = styled.div`
+  background-image: url(${background});
+  width: 100vw;
+  height: 100vh;
+`
 
