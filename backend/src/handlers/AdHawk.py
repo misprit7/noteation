427a7cb82a193e113a23fd53b5ef8c4f0895b633
Renu-R <aerorenu@gmail.com>
2022-09-17 23:47:32 -0700

    no , i dot care

--- backend/src/handlers/AdHawk.py
+++ backend/src/handlers/AdHawk.py
@@ -1,13 +1,71 @@
 import sys
+#sys.path.append('../')
 import adhawkapi
 import adhawkapi.frontend
 from adhawkapi import Events, MarkerSequenceMode, PacketType
 import time
+#from src.utils.GazeBuffer import GazeBuffer
+from src.classes.Event import Event
+#from src.classes.Database import Database
+from src.utils import utils
+import numpy as np
+import cv2
+
+class GazeBuffer:
+    MAX_SIZE = 87
+    def __init__(self):
+        self.buffer = []
+    
+    def append(self, array):
+        if len(self.buffer) < 20:
+            self.buffer.append(array)
+        else:
+            self.buffer.pop(0)
+            self.buffer.append(array)
+
+    def getBuffer(self):
+        return self.buffer
+    
+    def getCoordsForTimeStamp(self, timestamp):
+        timestamps = [row[0] for row in self.buffer]
+        idx = min(range(len(timestamps)), key=lambda i: abs(timestamps[i]-timestamp))
+        return [[self.buffer[idx][1], self.buffer[idx][2]],[self.buffer[idx-1][1], self.buffer[idx-1][2]]]
 
 
 class AdHawkHandler:
     #def __init__(self, handle_gaze_in_screen, handle_event_stream):
-    def __init__(self):
+    MARKER_DIC = cv2.aruco.DICT_4X4_50  # pylint: disable=no-member
+    ARUCO_MARKER_SIZE_MM = 20
+    ARUCO_MARKER_BORDER_MM = 10
+    EDGE_OFFSETS_MM = np.array([[10, 10], [10, 10]])  # Marker offsets: [[left, right], [top, bottom]]
+    SCREEN_WIDTH_CM = 35.93
+    SCREEN_HEIGHT_CM = 20.95
+    _screen_size_mm = [359.5, 209.5]
+
+    def _calculate_marker_positions(self):
+        margin_size = self.EDGE_OFFSETS_MM * 1e-3
+        screen_size = self._screen_size_mm * 1e-3
+        marker_size = self.ARUCO_MARKER_SIZE_MM * 1e-3
+
+        positions = np.array([
+            [margin_size[0, 0], - margin_size[1, 0] - marker_size],
+            [screen_size[0] - margin_size[0, 1] - marker_size, - margin_size[1, 0] - marker_size],
+            [margin_size[0, 0], - screen_size[1] + margin_size[1, 1]],
+            [screen_size[0] - margin_size[0, 1] - marker_size, - screen_size[1] + margin_size[1, 1]],
+        ])
+        markers = []
+        for marker_pos in positions:
+            markers.append([*marker_pos, marker_size])
+        return markers
+
+    def __init__(self, Database):
+
+        self.Database = Database
+        self._screen_size_mm = np.array([359.5, 209.5])
+        self._marker_positions = self._calculate_marker_positions()
+        self.MARKER_DIC = cv2.aruco.DICT_4X4_50
+        self._marker_ids = [0,1,2,3]
+        self.BUFFER = GazeBuffer()
         
         # Instantiate an API object
         self._api = adhawkapi.frontend.FrontendApi()
@@ -25,7 +83,6 @@ class AdHawkHandler:
 
         # Flags the frontend as not connected yet
         self.connected = False
-        self.Buffer = []
 
     def shutdown(self):
         ''' Shuts down the backend connection '''
@@ -60,6 +117,11 @@ class AdHawkHandler:
 
     def register_screen(self, screen_width, screen_height, aruco_dic, marker_ids, markers):
         ''' Registers the screen and starts tracking on a successful discovery'''
+        # screen_width = self._screen_size_mm[0]* 1e-3
+        # screen_height = self._screen_size_mm[1]* 1e-3
+        # marker_ids = self._marker_ids
+        # markers = self.positions
+        # aruco_dic = self.MARKER_DIC
 
         # Tells the api to search for the screen displaying ArUco (tracking) markers with the given parameters.
         # We set self._handle_screen_registered_response as the handler for the api's response to this request.
@@ -78,7 +140,7 @@ class AdHawkHandler:
             self._api.stop_screen_tracking(lambda *_args: None)
 
     def _handle_gaze_in_screen(self, _timestamp, xpos, ypos):
-        self.Buffer.append([_timestamp, xpos, ypos])
+        self.BUFFER.append([_timestamp, xpos, ypos])
         print([_timestamp, xpos, ypos])
     
     def _handle_event_stream(self, event_type, _timestamp, *_args):
@@ -86,32 +148,54 @@ class AdHawkHandler:
         #TODO: decide how to handle this since we want events to be handled to 
         #change the DB value. easy option: no calibration so no renabling needed
 
-        if event_type == Events.SACCADE:
-            print('saccade' + str(_timestamp) + str(_args[0]))
+        # if event_type == Events.SACCADE:
+        #     print('saccade' + str(_timestamp) + str(_args[0]))
+        #     print(_args)
+        #     print('before' + str(self.BUFFER.getCoordsForTimeStamp(_timestamp)[1]))
+        #     print('after' + str(self.BUFFER.getCoordsForTimeStamp(_timestamp)[0]))
+
+        #     before = self.BUFFER.getCoordsForTimeStamp(_timestamp)[1]
+        #     after = self.BUFFER.getCoordsForTimeStamp(_timestamp)[0]
+        #     fancy_ML_regression_fitting_magic = (after[1]-before[1]) - (after[0] - before[0])
+        #     print("g = " + str(fancy_ML_regression_fitting_magic))
+        #     if fancy_ML_regression_fitting_magic < 0:
+        #         print("TURN!!")
+
         if event_type == Events.TRACKLOSS_END:
             print(str(_timestamp) + str(_args))
 
-        if event_type == Events.PROCEDURE_ENDED:
+            if _args[0] == 0:
+                event = Event({"timestamp": time.time(), "command": "forward"})
+            else:
+                event = Event({"timestamp": time.time(), "command": "backward"})
 
+            utils.log_event(self.Database, event)
+
+        if event_type == Events.PROCEDURE_ENDED:
             # Screen tracking gets disabled when we start a marker sequence procedure, such as a Quick Start or
             # calibration, so we re-enable it upon receiving a PROCEDURE_ENDED event
             self.enable_screen_tracking(True)
 
     def _handle_camera_start_response(self, error):
         print('Camera started')
+        # screen_width = self._screen_size_mm[0]* 1e-3
+        # screen_height = self._screen_size_mm[1]* 1e-3
+        # marker_ids = self._marker_ids
+        # markers = self.positions
+        # aruco_dic = self.MARKER_DIC
         #TODO: hardcode the screen sizing stuff!!!!
         self.register_screen(self._screen_size_mm[0] * 1e-3, self._screen_size_mm[1] * 1e-3,
                                         self.MARKER_DIC, self._marker_ids, self._marker_positions)
 
-        # Handles the response after starting the tracker's camera
-        if error:
-            # End the program if there is a camera error
-            print(f'Camera start error: {error}')
-            self.shutdown()
-            sys.exit()
-        else:
-            # Otherwise, starts the video stream, streaming to the address of the video receiver
-            self._api.start_video_stream(*self._video_receiver_address, lambda *_args: None)
+        # # Handles the response after starting the tracker's camera
+        # if error:
+        #     # End the program if there is a camera error
+        #     print(f'Camera start error: {error}')
+        #     self.shutdown()
+        #     sys.exit()
+        # else:
+        #     # Otherwise, starts the video stream, streaming to the address of the video receiver
+        #     self._api.start_video_stream(*self._video_receiver_address, lambda *_args: None)
          
 
     def _handle_connect_response(self, error):
@@ -161,7 +245,8 @@ def main():
         while not handler.getState:
             pass  # Waits for the frontend to be connected before proceeding
 
-        input('Press Enter to run a Quick Start.')
+
+        #input('Press Enter to run a Quick Start.')
 
         # Runs a Quick Start at the user's command. This tunes the scan range and frequency to best suit the user's eye
         # and face shape, resulting in better tracking data. For the best quality results in your application, you
